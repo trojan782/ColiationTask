f135442ccd14694a772e5b26e8d599f2b64361a0
trojan782 <davidjacobs522@gmail.com>
2021-10-21 23:07:51 +0100

    gotten the dashboard page

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,5 +7,38 @@
         </h2>
     </x-slot>
 
-    
+    <!-- component -->
+    <div class="grid grid-cols-3 gap-2">
+<div class="max-w-md py-4 px-8 bg-white shadow-lg rounded-lg my-20">
+  <div>
+    <h2 class="text-gray-800 text-2xl font-semibold">Project Name</h2>
+    <p class="mt-2 text-gray-600">Description...</p>
+  </div>
+  <div class="flex justify-end mt-4">
+    <a href="#" class="text-xl font-medium text-indigo-500">View Tasks</a>
+  </div>
+</div>
+
+<div class="max-w-md py-4 px-8 bg-white shadow-lg rounded-lg my-20">
+  <div>
+    <h2 class="text-gray-800 text-2xl font-semibold">Project Name</h2>
+    <p class="mt-2 text-gray-600">Description...</p>
+  </div>
+  <div class="flex justify-end mt-4">
+    <a href="#" class="text-xl font-medium text-indigo-500">View Tasks</a>
+  </div>
+</div>
+
+<div class="max-w-md py-4 px-8 bg-white shadow-lg rounded-lg my-20">
+  <div>
+    <h2 class="text-gray-800 text-2xl font-semibold">Project Name</h2>
+    <p class="mt-2 text-gray-600">Description...</p>
+  </div>
+  <div class="flex justify-end mt-4">
+    <a href="#" class="text-xl font-medium text-indigo-500">View Tasks</a>
+  </div>
+</div>
+
+
+    </div>
 </x-app-layout>
