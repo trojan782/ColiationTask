8b54cc727f83b5efbf750f85bb45c61da9525176
trojan782 <davidjacobs522@gmail.com>
2021-10-25 00:41:12 +0100

    added the user model

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -10,13 +10,14 @@ class Project extends Model
     use HasFactory;
 
     protected $filable = [
+        'user_id',
         'title',
         'description',
         'task'
     ];
 
     public function users() {
-        return $this->belongsTo(User::class);
+        return $this->belongsTo('App\Models\User', 'user_id');
     }
 
     /**
