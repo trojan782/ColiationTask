cea4f49c4c1b6424c24cdccc4ab9c77aaf279e9f
trojan782 <davidjacobs522@gmail.com>
2021-10-25 00:20:52 +0100

    fixed the dropdown

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,11 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Task;
+use App\Models\Project;
 use Illuminate\Http\Request;
+use App\Http\Requests\TaskRequest;
+use Illuminate\Support\Facades\Auth;
 
 class TaskController extends Controller
 {
-    public function addTask() {
-        return view('newTask');
+
+     public function addTask()
+    {
+        //To get all the projects of the user
+        $projects = Project::where('user_id', Auth::id())->get();
+
+        return view('newTask', ['projects' => $projects]);
+    }
+
+    public function createTask(TaskRequest $request, Project $project) {
+        $project = $project->id;
+        $task = new Task();
+        $task->task = $request->task;
+        $task->projectId = $project->id;
+        $task->save();
+        dd($task);
+        return redirect()->back();
     }
 }
