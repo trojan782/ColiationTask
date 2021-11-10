d6e37b243e5201f913513c2baac3522cc180e248
trojan782 <davidjacobs522@gmail.com>
2021-11-10 15:06:50 +0100

    changed the csrf

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -8,7 +8,8 @@
         <x-jet-validation-errors class="mb-4" />
 
         <form method="POST" action="{{ route('register') }}">
-            @csrf
+            {{-- @csrf --}}
+            {{ csrf_field() }}
 
             <div>
                 <x-jet-label for="name" value="{{ __('Name') }}" />
